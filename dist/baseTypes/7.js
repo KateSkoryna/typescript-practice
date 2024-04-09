9897e34dae73eec8d214b517334283438bf9fd0b
“Kateryna <k.skoryna@gmail.com>
2024-04-09 16:56:53 +0200

    Add types to task 7

--- dist/baseTypes/7.js
+++ dist/baseTypes/7.js
@@ -1,5 +1,28 @@
 /*
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
-*/ 
+*/
+var WorkingDay;
+(function (WorkingDay) {
+    WorkingDay[WorkingDay["Monday"] = 0] = "Monday";
+    WorkingDay[WorkingDay["Tuesday"] = 1] = "Tuesday";
+    WorkingDay[WorkingDay["Wednesday"] = 2] = "Wednesday";
+    WorkingDay[WorkingDay["Thursday"] = 3] = "Thursday";
+    WorkingDay[WorkingDay["Friday"] = 4] = "Friday";
+})(WorkingDay || (WorkingDay = {}));
+var Weekend;
+(function (Weekend) {
+    Weekend[Weekend["Saturday"] = 0] = "Saturday";
+    Weekend[Weekend["Sunday"] = 1] = "Sunday";
+})(Weekend || (Weekend = {}));
+function isWeekend(day) {
+    if (WorkingDay[WorkingDay[day]]) {
+        return true;
+    }
+    else if (Weekend[Weekend[day]]) {
+        return false;
+    }
+    throw Error("Write correct day");
+}
+export {};
 //# sourceMappingURL=7.js.map
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,27 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum WorkingDay {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday
+}
+  
+enum Weekend {
+  Saturday,
+  Sunday
+}
+  
+  function isWeekend(day: string): boolean | never {
+    if (WorkingDay[WorkingDay[day]]) {
+      return true;
+    } else if (Weekend[Weekend[day]]) {
+      return false;
+    }
+      throw Error("Write correct day")
+  }
+
+
+export { };
